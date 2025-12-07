077b2f0bd124265a6e8cc3d197b7e53a59fb5255
Alan Davieli <alandavieli@icloud.com>
2025-12-06 21:56:58 -0600

    Corrected the bug where the dark mode wasn't showing. (That wasn't that big of a deal ok? Who needs the dark mode? People working at night? Go to sleep man)

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class', // Enable manual toggling
+  theme: {
+    extend: {
+      colors: {
+        gold: {
+          50: '#fbf8f1',
+          100: '#f5edd6',
+          200: '#ebd9aa',
+          300: '#dfc078',
+          400: '#d4a74b',
+          500: '#c68d2b',
+          600: '#aa6f20',
+          700: '#88531b',
+          800: '#71421b',
+          900: '#5e3619',
+        },
+        cream: {
+          50: '#fdfbf7',
+          100: '#fbf8f1',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Georgia', 'serif'],
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+      }
+    },
+  },
+  plugins: [],
+}
